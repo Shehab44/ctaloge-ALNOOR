bf2d3098e671888f11f4d682acbe051d61d42f55
Shehab44 <ehsanshehalab44@gmail.com>
2026-08-04 08:09:44 +0100

    اخر

--- tmp-sqlite-check.mjs
+++
@@ -1,7 +0,0 @@
-import { DatabaseSync } from 'node:sqlite';
-
-const db = new DatabaseSync('test.sqlite');
-db.exec('create table if not exists t(id integer primary key, name text)');
-db.exec("insert into t(name) values('ok')");
-console.log(db.prepare('select count(*) as c from t').get().c);
-db.close();
